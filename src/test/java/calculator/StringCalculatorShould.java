9b90036a56357bf8bf24706ac7fdb020ef69c371
Bhomesh <mrbhoge@gmail.com>
2021-10-17 22:22:32 +0530

    test case for empty string

--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -6,15 +6,11 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class StringCalculatorShould {
 
-    @Test
-    void empty_string_should_return_0() {
-        StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(0, stringCalculator.add(""));
-    }
+	StringCalculator stringCalculator = new StringCalculator();
 
-    @Test
-    void string_with_single_number_should_return_number_as_int() {
-        StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(1, stringCalculator.add("1"));
-    }
+	@Test
+	void empty_string_should_return_0() {
+
+		assertEquals(0, stringCalculator.add(""));
+	}
 }
