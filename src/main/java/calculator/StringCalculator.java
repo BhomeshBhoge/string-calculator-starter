6dc0dd02ce90c5eb2d73091422f0ddf677508b8a
Bhomesh <mrbhoge@gmail.com>
2021-10-17 22:14:04 +0530

    implementation of written test cases

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,9 +1,68 @@
 package calculator;
 
+import java.util.ArrayList;
+
 class StringCalculator {
 
     public int add(String input) {
-        return 0;
+    
+    	if (input.isEmpty()) {
+			return 0;
+		} else {
+			ArrayList<String> negativeNumbers = new ArrayList<String>();
+	
+			if(input.startsWith("//")) {
+				String s2 = input.replace("//", "");
+				String[] sample = s2.split("[\n]");
+				String newDelimeter = sample[0];
+				int sum1= 0;
+				String[] test = s2.split("[\n,"+newDelimeter+"]");
+				
+			
+				for(String s1: test) {
+					if(s1.equals(""))
+						continue;
+					int num = Integer.parseInt(s1);
+					if(num<0)
+						negativeNumbers.add(s1);
+				}
+				if(negativeNumbers.size()>0) {
+					throw new RuntimeException("negatives not allowed: " + String.join(", ",negativeNumbers));
+				}
+				for(String s:test) {
+					if(s.equals(""))
+						continue;
+					else if(Integer.parseInt(s)>1000)
+						continue;
+					sum1= sum1 + Integer.parseInt(s);
+				}
+				return sum1;
+			}else {
+				//when delimiter is ,
+			int sum1= 0;
+			String[] test = input.split("[\n,]");
+			
+			//check for negative number 
+			for(String s1: test) {
+				if(s1.equals(""))
+					continue;
+				int num = Integer.parseInt(s1);
+				if(num<0)
+					negativeNumbers.add(s1);
+			}
+			if(negativeNumbers.size()>0) {
+				throw new RuntimeException("negatives not allowed: " + String.join(", ",negativeNumbers));
+			}
+			for(String s:test) {
+				if(s.equals(""))
+					continue;
+				else if(Integer.parseInt(s)>1000)
+					continue;
+				sum1= sum1 + Integer.parseInt(s);
+			}
+			return sum1;
+			}
+		}
     }
 
 }
